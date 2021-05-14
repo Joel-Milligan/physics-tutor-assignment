40026c8653274827dd196d6ebdd3532df34d7201
lab2823 <leeblag@gmail.com>
2021-05-15 00:33:56 +0800

    added form classs

--- app/forms.py
+++ app/forms.py
@@ -13,6 +13,4 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
-    is_admin = BooleanField('give admin?(only click if you\'re actually an admin😛)')
-
-#class AnswerForm(FlaskForm):
+    is_admin = BooleanField('give admin?(only click if you\'re actually an admin)')
