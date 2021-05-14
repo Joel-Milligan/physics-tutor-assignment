ea79aa3fb0f7482cd79d7d9bcf3d27669d9937c8
lab2823 <leeblag@gmail.com>
2021-05-14 22:40:48 +0800

    added login page route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm, RegisterForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('LoginRegisterPage.html', login_form=LoginForm(), register_form=RegisterForm())
