f968777f7bdf69632e2350dedff3e3b9661eb609
Joel Milligan <22701252@student.uwa.edu.au>
2021-05-16 15:08:10 +0800

    Added a sprinkle of jQuery

--- app/static/js/LoginRegisterPage.js
+++ app/static/js/LoginRegisterPage.js
@@ -3,13 +3,13 @@ function openTab(evt, TabName) {
   var i, tabcontent, tablinks;
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = $(".tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = $(".tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
